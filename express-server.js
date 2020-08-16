e8b40dbdec4a62b4fc93d9d157af5940197d41e3
gabit <gabit690@gmail.com>
2020-08-16 13:03:21 -0300

    Enrutamiento

--- express-server.js
+++ express-server.js
@@ -2,10 +2,40 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello Express');
+app.use(express.json()); // Para que express entienda JSON
+
+// Creación de multiples rutas
+
+app.all('/user', (req, res, next) => {
+  console.log("Se ejecuto app.all");
+  next();
+});
+
+app.get('/user', (req, res) => {
+  res.json(
+      {
+          username: 'gabit',
+          age: 300
+      }
+  );
+});
+
+app.post('/user/:id', (req, res) => {
+    console.log(req.body);
+    console.log(req.params);
+    res.send('Peticion POST recibida');
+});
+
+app.put('/contact', (req, res) => {
+    res.send('Peticion PUT recibida');
+});
+
+app.delete('/user/:userID', (req, res) => {
+    res.send(`User ${req.params.userID} deleted`);
 });
 
 app.listen(3000, () => {
   console.log('Server Express on Port 3000');
 });
+
+//npx nodemon nombreDeArchivo.js
